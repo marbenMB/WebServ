0f185d8291af28c4294967d9d755b9a0f02dba37
ABOULHAJ <aboulhaj@student.1337.ma>
2023-03-13 14:09:34 +0100

    fixing of error '#' inside location scop and adding a default configfile and run it in case of calling only the './webserver'

--- source/config_file/save_data.cpp
+++ source/config_file/save_data.cpp
@@ -68,6 +68,8 @@ void ConfigFile::location(Data &g_Data, ServerConf &server, KeyValue v)
             trim(v.line, " \t'[]");
             if(v.line.length() == 0)
                 continue;
+            if (v.line[0] == '#')
+                continue;
             else {
                 v.value = v.line;
                 break;
@@ -79,6 +81,8 @@ void ConfigFile::location(Data &g_Data, ServerConf &server, KeyValue v)
             this->line_index++;
             trim(v.line, " \t:'[]");
             if(v.line.length() == 0)
+                continue;
+            if (v.line[0] == '#')
                 continue;
             if (v.line == "}")
                 break;
--- source/main.cpp
+++ source/main.cpp
@@ -9,6 +9,10 @@ int main (int ac, char **av)
         g_Data.configuration.parcing_file(av[1], g_Data);
         
     }
+    else if (ac == 1)
+    {
+        g_Data.configuration.parcing_file("def_configfile.conf", g_Data);
+    }
     else
         g_Data.error = "error in number of argument";
     if (!g_Data.error.empty())
