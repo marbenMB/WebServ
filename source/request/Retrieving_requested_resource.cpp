a462310b88b414bb2640c33a4ff314cdb730f37c
mohamed MASSTOUR <71995273+mmasstou@users.noreply.github.com>
2023-03-25 14:08:29 +0000

    remove unused file
    (;

--- source/request/Retrieving_requested_resource.cpp
+++ source/request/Retrieving_requested_resource.cpp
@@ -100,3 +100,151 @@ void    request::Retrieving_requested_resource(ServerConf *server)
         (_findHeader(REQUEST_METHOD).compare("DELETE")))
             this->__noImplimented = ALLOWED;
 }
+
+void request::checkForIndex(std::vector<std::string> vect){
+    if (vect.size()){
+        std::vector<std::string>::iterator it = vect.begin();
+        this->default_index.clear() ;
+        if (!it[0].empty()){
+            this->default_index.append("/");
+            this->default_index.append(*it);
+        }
+        else this->default_index.append("/index.html");
+        }
+    // std::cout << "in Default Index : " << it[0] << std::endl;
+    // std::cout << "out Default Index : " << this->default_index << std::endl;
+}
+void request::checkForErrorPage(std::vector<std::string> vect){
+    struct stat STATFile;
+    std::vector<std::string>::iterator errorspage_iterator = vect.begin();
+    int NbeOfPages = 0;
+    bool one = false, two = false, three = false, fore = false, five = false;
+    while (errorspage_iterator != vect.end())
+    {
+        if (((*errorspage_iterator).find(".html") == std::string::npos)){
+            if ((*errorspage_iterator)[0] == '1') one = true;
+            if ((*errorspage_iterator)[0] == '2') two = true;
+            if ((*errorspage_iterator)[0] == '3') three = true;
+            if ((*errorspage_iterator)[0] == '4') fore = true;
+            if ((*errorspage_iterator)[0] == '5') five = true;
+            // std::cout << "type :"<< (*errorspage_iterator)[0]<< std::endl;
+        }
+        if ((*errorspage_iterator).find(".html") != std::string::npos){
+            if (one) {
+
+                this->default_10x = this->root + "/" + *errorspage_iterator;
+                if (stat(this->default_10x.c_str(), &STATFile) != 0){
+                   this->default_10x = ERROR_PATH;
+                }
+            }
+            if (two){
+                 this->default_20x = this->root+ "/" + *errorspage_iterator;
+                if (stat(this->default_20x.c_str(), &STATFile) != 0){
+                   this->default_20x = ERROR_PATH;
+                }
+            }
+            if (three){
+                 this->default_30x = this->root + "/" + *errorspage_iterator;
+                if (stat(this->default_30x.c_str(), &STATFile) != 0){
+                   this->default_30x = ERROR_PATH;
+                }
+            }
+            if (fore){
+                this->default_40x = this->root + "/" + *errorspage_iterator;
+                if (stat(this->default_40x.c_str(), &STATFile) != 0){
+                   this->default_40x = ERROR_PATH;
+                }
+            }
+            if (five){
+                this->default_50x = this->root + "/" + *errorspage_iterator;
+                if (stat(this->default_50x.c_str(), &STATFile) != 0){
+                   this->default_50x = ERROR_PATH;
+                }
+            }
+            one = two = three = fore = five = false;
+            NbeOfPages++;
+        }
+        ++errorspage_iterator;
+    }  
+}
+
+int request::findLocation(std::vector<std::map<std::string, std::map<std::string, std::vector<std::string> > > > location){
+    char realPATH_dir[PATH_MAX];
+    char realPATH_subdir[PATH_MAX];
+    // char* _subdir;
+    std::vector<std::map<std::string, std::map<std::string, std::vector<std::string> > > >::iterator locations_iterator = location.begin();
+    int locationId = -1;
+    int index = 0;
+    std::string router;
+    size_t pos = 0;
+    
+    if ((pos =  _findHeader(REQUEST_URI).rfind(".py")) != std::string::npos && (pos + 3) ==  _findHeader(REQUEST_URI).length())
+    {
+       this->is_cgi = true; 
+        this->compare_URI.append("\%.py$");
+    }
+    else if ((pos =  _findHeader(REQUEST_URI).rfind(".go")) != std::string::npos && (pos + 3) ==  _findHeader(REQUEST_URI).length())
+    {
+        this->is_cgi = true;
+        this->compare_URI.append("\%.go$");
+    }
+    else  
+    {
+        // this->compare_URI.append("/");
+        // if (!this->request_URI.empty())
+        this->compare_URI.append(this->_findHeader(REQUEST_URI));
+    }
+    // this->compare_URI.append(this->request_URI);
+     //  * init string |> /srcs/dir001/dir0011/test.txt
+    // std::cout << "++> URI :" << this->compare_URI << std::endl;
+
+    while (locations_iterator != location.end())
+    {
+        // ***> Create iterator for location Data
+        std::map<std::string, std::map<std::string, std::vector<std::string> > >::iterator location_iterator = locations_iterator->begin();
+        // std::cout << "  +>" << location_iterator->first << "<| >this->compare_URI :" << this->compare_URI<<"<|" << std::endl;
+        router.clear();
+        router.append(location_iterator->first);
+        // std::cout << "router : |" << router << "| this->compare_URI : |" << this->compare_URI <<"| " <<  std::endl;
+        if (router.compare(this->compare_URI) == 0){locationId = index;break;}
+        ++locations_iterator;
+        index++;
+        if (locations_iterator == location.end() && locationId == -1){  // if '/srcs/dir001/dir0011/test.txt' not found ;
+            size_t start = this->compare_URI.find_last_of("/"); // find the last '/'
+            // std::cout << "  #|>" << start <<std::endl;
+            if (start != std::string::npos && this->compare_URI.compare("/") != 0){ // if the's  '/'
+                locations_iterator = location.begin(); // init iterator to the begin
+                index = 0; // init index count to the 0 for anther tour
+                if (!start){start += 1;}// for the last one means request URI is  '/' ;becuse we should remove the last '/'
+                this->compare_URI.erase(start, this->compare_URI.length() - 1); // now remove after ''/
+            }
+        }
+    }
+    if (!this->is_cgi) // remove the mutch string 
+    {
+        if (locationId != -1)
+        {
+            std::string __erraseTmp( _findHeader(REQUEST_URI)) ;
+            std::string __URI(this->getroot());
+            if (__erraseTmp.compare("/") != 0)
+            {
+                this->setcompare_URI(router);
+                __erraseTmp.erase(0,router.length());
+                __URI.append("/");
+            }
+            __URI.append(__erraseTmp);
+            realpath("./", realPATH_subdir);
+            realpath(__URI.c_str(), realPATH_dir);
+            __erraseTmp.clear();
+            __erraseTmp.append(realPATH_dir);
+            __erraseTmp.erase(0, strlen(realPATH_subdir));
+            __URI.clear();
+            __URI.append(".");
+            __URI.append(__erraseTmp);
+            this->_requestHeaders[REQUEST_URI] = __URI;
+        }
+    }
+    if (this->is_cgi && locationId == -1)
+        throw  _Exception(NOT_IMPLEMENTED);
+    return locationId;
+}
